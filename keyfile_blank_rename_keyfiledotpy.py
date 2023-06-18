e9b371f1e88566ec915a52a0f23280ddcd22619c
SqrGame <xisle35@gmail.com>
2023-06-18 16:28:47 -0400

    committed to excellence

---
+++ keyfile_blank_rename_keyfiledotpy.py
@@ -0,0 +1,21 @@
+LeoKey = "Your Leonardo.ai key"
+LeoBearer = "Your Leonardo.ai Bearer"
+OpenAikey = "Your Open AiKey"
+
+#tweeting
+tlogin_user = 'TwitterUserName'
+tlogin_pass = 'TwitterPassword'
+tapi_key = 'TwitterApiKey'
+tapi_secret = 'TwitterApiSecretKey'
+tbearer_token = 'TwitterBearerToken'
+taccess_token = 'TwitterAccessToken'
+taccess_token_secret = 'TwitterAccessTokenSecret'
+tClient_ID = 'TwitterClientID'
+tClient_Secret = 'TwitterClientSecret'
+#google
+GoogleApi = 'Google API Key'
+GoogleClientID = 'Google Client ID'
+GoogleClientSecret = 'Google Client Secret'
+#Word Press
+WPUSer = 'wpUser Name'
+WPPw = 'Wp Password'
